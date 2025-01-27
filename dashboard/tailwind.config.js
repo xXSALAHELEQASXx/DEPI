15f416ca63ec735528a0953b26e10ad1b6c15890
xXSALAHELEQASXx <mohamed_1440a@fci.kfs.edu.eg>
2025-01-27 19:40:16 +0300

    Merged remote changes

---
+++ dashboard/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Make sure to include all the relevant file extensions you're using in your React app
+  ],
+  darkMode: false, // or 'media' or 'class' if you're using dark mode
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
