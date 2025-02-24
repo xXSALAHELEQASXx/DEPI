22a3c7c62e0d92d9ce2bf275ac9d305f56366776
Mohamed Salah <164447928+xXSALAHELEQASXx@users.noreply.github.com>
2025-02-24 12:34:48 +0200

    Update App.js

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,23 +1,35 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
-import StatisticsPage from './pages/StatisticsPage';
-import MainPage from './pages/MainPage';
-import './styles/App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import AudiencePage from "./pages/AudiencePage";
+import StatisticsPage from "./pages/StatisticsPage";
+import ChatbotPage from "./pages/ChatbotPage";
+import CalendarPage from "./pages/CalendarPage";
+import MainPage from "./pages/MainPage";
+
+import "./styles/App.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <Sidebar />
-      <main style={{ marginLeft: '250px', padding: '20px' }}>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/statistics" element={<StatisticsPage />} />
-        </Routes>
-      </main>
-    </div>
+    <BrowserRouter> 
+      <div className="App">
+        <Header />
+        <Sidebar />
+        <main style={{ marginLeft: "250px", padding: "20px" }}>
+          <h2>Welcome to the Dashboard</h2>
+          <Routes>
+            <Route path="/AudiencePage" element={<AudiencePage />} /> 
+            <Route path="/StatisticsPage" element={<StatisticsPage />} />
+            <Route path="/chatbotpage" element={<ChatbotPage />} />
+            <Route path="/CalendarPage" element={<CalendarPage/>} />
+            <Route path="/MainPage" element={<MainPage/>} />
+        
+        
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 };
 
