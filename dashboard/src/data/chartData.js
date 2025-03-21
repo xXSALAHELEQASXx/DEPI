8dc42c7ba9084e6d7c6a26c1da8c22d68c74cf9c
Mohamed Salah <164447928+xXSALAHELEQASXx@users.noreply.github.com>
2025-03-22 02:55:03 +0300

    Add files via upload

---
+++ dashboard/src/data/chartData.js
@@ -0,0 +1,13 @@
+const chartData = {
+  labels: ["USA", "UK", "Germany", "France", "Egypt"], 
+  datasets: [
+    {
+      label: "Followers by Region",
+      data: [500, 400, 350, 300, 450],
+      backgroundColor: "#9575CD",
+      borderRadius: 5,
+    },
+  ],
+};
+
+export default chartData;
